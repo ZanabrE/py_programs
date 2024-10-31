ca554697d513425081f0934208ca46f8c5bad5ae
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2024-10-30 20:21:35 -0400

    modified: workbench.py

--- workbench.py
+++ workbench.py
@@ -7,5 +7,18 @@
 #   Your telephone number
 #   Your college major
 
-name = input("Enter you name: ")
-print(name)
+name = input("Enter your name: ")
+address = input("Enter your address: ")
+city = input("Enter your city: ")
+state = input("Enter your state: ")
+zip = int(input("Enter your ZIP code: "))
+telephone = int(input("Enter your telephone: "))
+college_major = input("Enter you college major: ")
+print('Hello', name)
+print("This is your information ")
+print(address)
+print(city)
+print(state)
+print(zip)
+print(telephone)
+print(college_major)
