67c80d8c8218509871081e0716790f8fa7b00c73
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2024-10-30 20:11:40 -0400

    new file: workbench.py

---
+++ workbench.py
@@ -0,0 +1,11 @@
+#This file will have several programs. 
+
+#1. Personal Information
+#   Write a program that displays the following information:
+#   Your name
+#   Your address, with city, state, and ZIP
+#   Your telephone number
+#   Your college major
+
+name = input("Enter you name: ")
+print(name)
