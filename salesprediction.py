f6b804e33da7a531ae30cc05329a718e615e8faa
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2024-10-30 20:31:48 -0400

    new file: salesprediction.py

---
+++ salesprediction.py
@@ -0,0 +1,10 @@
+#A company has determine that its annual profit is typically 23 percent
+#of total sales. Write a program that asks the user to enter the projected 
+#amount of total sales, then display the profit that will be made from the amount.
+#Hint: Use the value 0.23 to represent 23 percent.
+
+sales = int(input("Enter the projected sales: "))
+
+annual_profit = sales * 0.23
+
+print("The profit is ", annual_profit)
