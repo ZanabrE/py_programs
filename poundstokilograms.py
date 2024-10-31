29b784507d492e1e2f799fb0759a4efb6d4866f5
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2024-10-30 21:36:19 -0400

    modified: poundstokilograms.py

--- poundstokilograms.py
+++ poundstokilograms.py
@@ -2,3 +2,8 @@
 #the mass of an object in pounds and then calculates and displays the mass of the object 
 #in kilograms.
 
+mass = int(input("Enter the mass of the object in pounds: "))
+
+convertion = mass * 0.454
+
+print("The mass of the object in killograms is ", format(convertion, '.2f'), "Kg")
