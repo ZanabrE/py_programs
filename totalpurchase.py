18b194272ac4864b88b60d9bdd632eaf76c2c75c
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2024-11-02 10:34:44 -0400

    modified: totalpurchase.py

---
+++ totalpurchase.py
@@ -0,0 +1,4 @@
+#A customer in a store is purchasing five items. Write a program that asks for the price 
+#of each item, then displays the subtotal of sale, the amount of sales tax, and the total.
+#Assume that sales tax is 7 percent.
+
