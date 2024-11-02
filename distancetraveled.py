82c262af7c76a6fed837c537b75b56ea69534ead
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2024-11-02 12:14:37 -0400

    new file: distancetraveled.py

---
+++ distancetraveled.py
@@ -0,0 +1,15 @@
+#Assuming there are no accidents or delays, the distance that a car travels down the interstate
+#can be calculated with the following fomula:
+#       Distance = Speed x Time
+#A car is traveling at 70 miles per hour. Write a program that displays the following:
+#The distance the car will travel in 6 hours
+#The distance the car will travel in 10 hours
+#The distance the car will travel 15 hours
+
+#Variable
+miles_per_hour = 70
+
+#Displaying the distance the car will travel
+print("The distance the car will travel in 6 hours is ", str(miles_per_hour * 6), 'miles')
+print("The distance the car will travel in 10 hours is ", str(miles_per_hour * 10), 'miles')
+print("The distance the car will travel in 15 hours is ", str(miles_per_hour * 15), 'miles')
