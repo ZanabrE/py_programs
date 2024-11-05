b31d1acaff7242fa832625173df0e91f8f66b0f9
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2024-11-05 18:59:08 -0500

    modified: number_analyser_3.py

--- number_analyser_3.py
+++ number_analyser_3.py
@@ -3,3 +3,21 @@
 #and "Zero" if the number is equal to 0. The program should then display "Even" if the number
 #is even, and "Odd" if the number is odd. 
 
+#User input
+number = int(input("Enter an integer number: "))
+
+#Conditions
+if number > 0:
+    print("Positive")
+elif number < 0:
+    print("Negative")
+elif number == 0:
+    print("Zero")
+
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+    
+#Displaying the output:
+print("The number is ", number)
