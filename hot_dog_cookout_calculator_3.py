f62fb4e91b847f3c9fe0f38c768bbed3c5982856
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2024-11-11 12:30:35 -0500

    modified: hot_dog_cookout_calculator_3.py

--- hot_dog_cookout_calculator_3.py
+++ hot_dog_cookout_calculator_3.py
@@ -7,3 +7,24 @@
 #• The minimum number of packages of hot dog buns required
 #• The number of hot dogs that will be left over
 #• The number of hot dog buns that will be left over
+
+#Variables
+HOT_DOGS = 10
+HOT_DOGS_BUNS = 8
+
+message = ''
+
+number_of_people = int(input("How many people will be attending to the cookout: "))
+hot_dogs_per_person = int(input("How many hot dogs each person will be given: "))
+
+#Calculations
+total_hot_dogs = number_of_people * hot_dogs_per_person
+number_hot_dogs_required = total_hot_dogs / HOT_DOGS
+number_hot_dogs_buns_required = total_hot_dogs / HOT_DOGS_BUNS
+number_hot_dogs_left_over = total_hot_dogs % HOT_DOGS
+number_hot_dogs_buns_left_over = total_hot_dogs % HOT_DOGS_BUNS
+
+print('The minimum number of packages of hot dogs required ', number_hot_dogs_required)
+print('The minimum number of packages of hot dog buns required ', number_hot_dogs_buns_required)
+print('The number of hot dogs will be left over are ', number_hot_dogs_left_over)
+print('The number of hot dog buns that will be left over are ', number_hot_dogs_buns_left_over)
