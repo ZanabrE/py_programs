15b7f77609210e2eea61cc0473029bd26d02fca4
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2024-11-18 22:38:03 -0500

    modified: software_sales_3.py

--- software_sales_3.py
+++ software_sales_3.py
@@ -9,3 +9,39 @@
 #should then display the amount of the discount (if any) and the total amount of the
 #purchase after the discount.
 
+#Variable
+package_price = 99
+
+#user input
+number_of_package = float(input('Enter the number of package you wish to buy: '))
+
+#Conditions
+if number_of_package < 0:
+    print('Error. Please enter a number of packages greater than 0.')
+else:
+    discount = 0
+    
+    if number_of_package < 10:
+        discount = 0
+        
+    elif number_of_package >= 10 and number_of_package <= 19:
+        discount = .10
+        
+    elif number_of_package >= 20 and number_of_package <= 49:
+        discount = .20
+    
+    elif number_of_package >= 50 and number_of_package <= 99:
+        discount = .30
+        
+    elif number_of_package >= 100:
+        discount = .40
+    
+#Calculations
+total_price = number_of_package * package_price
+total_discount = (total_price) * discount
+total_amount = total_price - total_discount
+
+print('You have bought ', number_of_package, ' packages and you will get a ', format(discount,'.0%'), 'discount')
+print('Your total is ', total_price)
+print('Your total amount is ', total_amount)
+    
