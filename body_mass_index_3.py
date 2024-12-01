dd908b025ba4c92a4cbb6104d0b8466d380fa2c4
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2024-12-01 12:03:57 -0500

    modified: body_mass_index_3.py

--- body_mass_index_3.py
+++ body_mass_index_3.py
@@ -9,3 +9,20 @@
 #BMI is between 18.5 and 25. If the BMI is less than 18.5, the person is considered to be
 #underweight. If the BMI value is greater than 25, the person is considered to be overweight.
 
+#User input
+weight = float(input('Enter your weight (in pounds): '))
+height = float(input('Enter your height (in inches): '))
+
+#Calculations.
+BMI = (weight * 703) / (height**2)
+
+if BMI >= 18.5 and BMI <= 25:
+    print('You are considered Optimal weight')
+if BMI < 18.5:
+    print ('You are considered Underweight')
+elif BMI > 25:
+    print ('You are considered Overweight')
+
+print('Your BMI is: ' + format(BMI, ',.2f'))
+
+
