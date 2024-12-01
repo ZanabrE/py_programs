1b97720612ceb33a69c42dd74a8d446dd67cd409
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2024-12-01 10:51:36 -0500

    modified: shipping_charges_3.py

--- shipping_charges_3.py
+++ shipping_charges_3.py
@@ -7,3 +7,18 @@
 #Write a program that asks the user to enter the weight of a package then displays the shipping
 #charges.
 
+weight_of_package = float(input('Enter the weight of a package: '))
+
+if weight_of_package <= 2:
+    rate_per_pound = 1.50
+elif weight_of_package >= 2 and weight_of_package <= 6:
+    rate_per_pound = 3.00
+elif weight_of_package >= 6 and weight_of_package <= 10:
+    rate_per_pound = 4.00
+elif weight_of_package > 10:
+    rate_per_pound = 4.75
+    
+#Calculations
+shipping_charges = weight_of_package * rate_per_pound
+
+print('Shipping charges are: ', format(shipping_charges, ',.2f'))
