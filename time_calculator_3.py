f601d1ba3ae6810c5228b03a0592b6bdb2b2ad2d
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2024-12-03 21:41:23 -0500

    modified: time_calculator_3.py

--- time_calculator_3.py
+++ time_calculator_3.py
@@ -9,3 +9,37 @@
 #greater than or equal to 86,400, the program should convert the number of seconds to
 #days, hours, minutes, and seconds.
 
+#User input
+number_of_seconds = int(input('Enter number of seconds that will be converted: '))
+
+message = ''
+
+#Conditions
+if number_of_seconds < 0:
+    message = 'Error. Please enter a number greater than 0.'
+
+if number_of_seconds >= 0 and number_of_seconds < 60:
+    seconds = format(number_of_seconds % 60)
+    message = 'Seconds: ' + seconds 
+elif number_of_seconds >= 60 and number_of_seconds < 3600:
+    minutes = format(number_of_seconds // 60)
+    seconds = format(number_of_seconds % 60)
+    message = 'Minutes: ' + minutes + ' ' \
+              'Seconds: ' + seconds
+elif number_of_seconds >= 3600 and number_of_seconds < 86400:
+    hours = format(number_of_seconds // 3600)
+    minutes = format((number_of_seconds % 3600) // 60)
+    seconds = format((number_of_seconds % 3600) % 60)
+    message = 'Hours: ' + hours + ' ' + \
+              'Minutes: ' + minutes + ' ' + \
+              'Seconds: ' + seconds
+elif number_of_seconds >= 86400:
+    days = format(number_of_seconds // 86400)
+    hours = format((number_of_seconds % 86400) // 3600)
+    minutes = format(((number_of_seconds % 86400) % 3600) // 60)
+    seconds = format(((number_of_seconds % 86400) % 3600) % 60)
+    message = 'Days: ' + days + ' ' + \
+              'Hours: ' + hours + ' ' + \
+              'Minutes: ' + minutes + ' ' + \
+              'Seconds: ' + seconds + ' '
+print(message)
