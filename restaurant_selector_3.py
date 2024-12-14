b678517aa6fa4f6aa94c62bf17a5e062611131d0
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2024-12-14 11:59:16 -0500

    new file: restaurant_selector_3.py

---
+++ restaurant_selector_3.py
@@ -0,0 +1,31 @@
+#You have a group of friends coming to visit for your high school reunion, and you want
+#to take them out to eat at a local restaurant. You aren’t sure if any of them have dietary
+#restrictions, but your restaurant choices are as follows:
+#
+#Joe’s Gourmet Burgers—Vegetarian: No, Vegan: No, Gluten-Free: No
+#Main Street Pizza Company—Vegetarian: Yes, Vegan: No, Gluten-Free: Yes
+#Corner Café—Vegetarian: Yes, Vegan: Yes, Gluten-Free: Yes
+#Mama’s Fine Italian—Vegetarian: Yes, Vegan: No, Gluten-Free: No
+#The Chef’s Kitchen—Vegetarian: Yes, Vegan: Yes, Gluten-Free: Yes
+#
+#Write a program that asks whether any members of your party are vegetarian, vegan, or
+#gluten-free, to which then displays only the restaurants to which you may take the group.
+#Here is an example of the program’s output:
+#Is anyone in your party a vegetarian? yes Enter
+#Is anyone in your party a vegan? no Enter
+#Is anyone in your party gluten-free? yes Enter
+#
+#Here are your restaurant choices:
+#   Main Street Pizza Company
+#   Corner Cafe
+#   The Chef's Kitchen
+#
+#Here is another example of the program’s output:
+#
+#Is anyone in your party a vegetarian? yes Enter
+#Is anyone in your party a vegan? yes Enter
+#Is anyone in your party gluten-free? yes Enter
+#
+#Here are your restaurant choices:
+#   Corner Cafe
+#   The Chef's Kitchen
